546297b91def9d8e75e8187233a80630e6a3fd6c
Tanimal <poyuncheng.bob@gmail.com>
2024-08-20 13:40:19 +0800

    modified

--- Summative/Constant.py
+++ Summative/Constant.py
@@ -3,4 +3,35 @@ Conditions = ["NormalBed", "ActuatedBed"]
 Directions = ["Right", "Up", "Left", "Down"]
 Applications = ["Ecosphere", "Archery"]
 EcosphereClips = ["EcosphereBorneo", "EcosphereRajaAmpat"]
-Colors = {"Standing": ["#54bebe", "#98d1d1"], "Lying": ["#c80064", "#d7658b"]}
+Colors = [["#54bebe", "#98d1d1"], ["#c80064", "#d7658b"]]
+
+RangeRadarChartAngleLabels = {
+    "LR": [
+        "Right 90",
+        "Right 60",
+        "Right 30",
+        "Front",
+        "Left 30",
+        "Left 60",
+        "Left 90",
+        "Left 120",
+        "Left 150",
+        "Back",
+        "Right 150",
+        "Right 120",
+    ],
+    "UD": [
+        "Front",
+        "Up 30",
+        "Up 60",
+        "Up 90",
+        "Up 120",
+        "Up 150",
+        "Back",
+        "Down 150",
+        "Down 120",
+        "Down 90",
+        "Down 60",
+        "Down 30",
+    ]
+}
