ff1384e09e0f7d2477c48ea7fbdb376b2b21bd1f
Tanimal <Bobcheng.bob@gmail.com>
2024-08-30 14:47:34 +0800

    add ttest result for likert

--- Summative/Freeplay_DrawLikert.py
+++ Summative/Freeplay_DrawLikert.py
@@ -2,6 +2,7 @@ import pygsheets
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import ttest_rel
 from Constant import *
 
 # Get dataframe from Google Sheet
@@ -33,50 +34,51 @@ for app in Applications:
 print(ComfortData)
 
 # Draw Comfort Figure
-fig, axes = plt.subplots(figsize=(12, 4), ncols=2, sharey=True)
-fig.tight_layout()
+def DrawComfortFig():
+    fig, axes = plt.subplots(figsize=(12, 4), ncols=2, sharey=True)
+    fig.tight_layout()
 
-for i, condition in enumerate(reversed(Conditions)):
-    data = np.array(list(ComfortData[condition].values()))
-    data_cum = data.cumsum(axis=1)
+    for i, condition in enumerate(reversed(Conditions)):
+        data = np.array(list(ComfortData[condition].values()))
+        data_cum = data.cumsum(axis=1)
 
-    axes[i].set_title(condition, color=Colors[(i+1)%2][4])
-    axes[i].set_xlim(0, 14)
-    axes[i].set_xticks([0, 2, 4, 6, 8, 10, 12, 14])
+        axes[i].set_title(condition, color=Colors[(i+1)%2][4])
+        axes[i].set_xlim(0, 14)
+        axes[i].set_xticks([0, 2, 4, 6, 8, 10, 12, 14])
 
-    for j in range(0, len(Points)):
-        widths = data[:, j]
-        starts = data_cum[:, j] - widths
-        rects = axes[i].barh(
-            Applications,
-            widths,
-            left=starts,
-            align="center",
-            height=0.4,
-            color=Colors[(i+1)%2][j],
-        )
-        for rect in rects:
-            x, y = rect.get_xy()
-            w, h = rect.get_width(), rect.get_height()
-            if (w == 0 or x == 0):
-                continue
-            axes[i].plot([x, x], [y + 0.01, y + h - 0.01], color='black', lw=1)
+        for j in range(0, len(Points)):
+            widths = data[:, j]
+            starts = data_cum[:, j] - widths
+            rects = axes[i].barh(
+                Applications,
+                widths,
+                left=starts,
+                align="center",
+                height=0.4,
+                color=Colors[(i+1)%2][j],
+            )
+            for rect in rects:
+                x, y = rect.get_xy()
+                w, h = rect.get_width(), rect.get_height()
+                if (w == 0 or x == 0):
+                    continue
+                axes[i].plot([x, x], [y + 0.01, y + h - 0.01], color='black', lw=1)
 
-axes[0].invert_xaxis()
-axes[0].set_yticks(range(len(Applications)))
-axes[0].set_yticklabels(Applications)
-axes[0].yaxis.tick_left()
-axes[0].tick_params(axis="y")
+    axes[0].invert_xaxis()
+    axes[0].set_yticks(range(len(Applications)))
+    axes[0].set_yticklabels(Applications)
+    axes[0].yaxis.tick_left()
+    axes[0].tick_params(axis="y")
 
-plt.ylim(-1, 3)
-plt.subplots_adjust(wspace=0, top=0.85, bottom=0.1, left=0.18, right=0.95)
-plt.suptitle("Comfort", fontsize=16, fontweight="bold")
+    plt.ylim(-1, 3)
+    plt.subplots_adjust(wspace=0, top=0.85, bottom=0.1, left=0.18, right=0.95)
+    plt.suptitle("Comfort", fontsize=16, fontweight="bold")
 
-figurepath = f"{RootDir}/Result Figure/Summative Freeplay Comfort [BarH].png"
-plt.savefig(figurepath, transparent=False)
-plt.close()
+    figurepath = f"{RootDir}/Result Figure/Summative Freeplay Comfort [BarH].png"
+    plt.savefig(figurepath, transparent=False)
+    plt.close()
 
-print(f"Figure saved to {ColorText(figurepath, "green")}\n")
+    print(f"Figure saved to {ColorText(figurepath, "green")}\n")
 
 # Get Preference Data
 PreferenceData = dict()
@@ -97,47 +99,61 @@ for i, row in df.iterrows():
 print(PreferenceData)
 
 # Draw Preference Figure
-fig, axes = plt.subplots(figsize=(12, 4), ncols=2, sharey=True)
-fig.tight_layout()
+def DrawPreferenceFig():
+    fig, axes = plt.subplots(figsize=(12, 4), ncols=2, sharey=True)
+    fig.tight_layout()
 
-for i, condition in enumerate(reversed(Conditions)):
-    data = np.array(list(PreferenceData[condition].values()))
-    data_cum = data.cumsum(axis=1)
+    for i, condition in enumerate(reversed(Conditions)):
+        data = np.array(list(PreferenceData[condition].values()))
+        data_cum = data.cumsum(axis=1)
 
-    axes[i].set_title(condition, color=Colors[(i+1)%2][4])
-    axes[i].set_xlim(0, 14)
-    axes[i].set_xticks([0, 2, 4, 6, 8, 10, 12, 14])
+        axes[i].set_title(condition, color=Colors[(i+1)%2][4])
+        axes[i].set_xlim(0, 14)
+        axes[i].set_xticks([0, 2, 4, 6, 8, 10, 12, 14])
 
-    for j in range(0, len(Points)):
-        widths = data[:, j]
-        starts = data_cum[:, j] - widths
-        rects = axes[i].barh(
-            ["Overall"] + Applications,
-            widths,
-            left=starts,
-            align="center",
-            height=0.4,
-            color=Colors[(i+1)%2][j],
-        )
-        for rect in rects:
-            x, y = rect.get_xy()
-            w, h = rect.get_width(), rect.get_height()
-            if (w == 0 or x == 0):
-                continue
-            axes[i].plot([x, x], [y + 0.01, y + h - 0.01], color='black', lw=1)
+        for j in range(0, len(Points)):
+            widths = data[:, j]
+            starts = data_cum[:, j] - widths
+            rects = axes[i].barh(
+                ["Overall"] + Applications,
+                widths,
+                left=starts,
+                align="center",
+                height=0.4,
+                color=Colors[(i+1)%2][j],
+            )
+            for rect in rects:
+                x, y = rect.get_xy()
+                w, h = rect.get_width(), rect.get_height()
+                if (w == 0 or x == 0):
+                    continue
+                axes[i].plot([x, x], [y + 0.01, y + h - 0.01], color='black', lw=1)
 
-axes[0].invert_xaxis()
-axes[0].set_yticks(range(len(Applications) + 1))
-axes[0].set_yticklabels(["Overall"] + Applications)
-axes[0].yaxis.tick_left()
-axes[0].tick_params(axis="y")
+    axes[0].invert_xaxis()
+    axes[0].set_yticks(range(len(Applications) + 1))
+    axes[0].set_yticklabels(["Overall"] + Applications)
+    axes[0].yaxis.tick_left()
+    axes[0].tick_params(axis="y")
 
-plt.ylim(-1, 3)
-plt.subplots_adjust(wspace=0, top=0.85, bottom=0.1, left=0.18, right=0.95)
-plt.suptitle("Preference", fontsize=16, fontweight="bold")
+    plt.ylim(-1, 3)
+    plt.subplots_adjust(wspace=0, top=0.85, bottom=0.1, left=0.18, right=0.95)
+    plt.suptitle("Preference", fontsize=16, fontweight="bold")
 
-figurepath = f"{RootDir}/Result Figure/Summative Freeplay Preference [BarH].png"
-plt.savefig(figurepath, transparent=False)
-plt.close()
+    figurepath = f"{RootDir}/Result Figure/Summative Freeplay Preference [BarH].png"
+    plt.savefig(figurepath, transparent=False)
+    plt.close()
 
-print(f"Figure saved to {ColorText(figurepath, "green")}\n")
+    print(f"Figure saved to {ColorText(figurepath, "green")}\n")
+
+
+def TTestAnalysis():
+    with open(f"{RootDir}/Processed Data/Summative_Freeplay_Likert_pval.txt", "w") as file:
+        for app in Applications:
+            file.write(f"Application: {app}\n")
+            t, p = ttest_rel(ComfortData["ActuatedBed"][app], ComfortData["NormalBed"][app])
+            file.write(f"Comfort: t = {t}, p = {p}\n")
+            t, p = ttest_rel(PreferenceData["ActuatedBed"][app], PreferenceData["NormalBed"][app])
+            file.write(f"Preference: t = {t}, p = {p}\n")
+            file.write("\n")
+
+TTestAnalysis()
