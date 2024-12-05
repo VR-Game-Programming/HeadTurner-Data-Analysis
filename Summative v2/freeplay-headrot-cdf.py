ef8bad3a66815f69c8407420188aac0a1bd2b444
Tanimal <Bobcheng.bob@gmail.com>
2024-12-05 21:11:31 +0800

    add gap_50 gap_90

--- Summative v2/freeplay-headrot-cdf.py
+++ Summative v2/freeplay-headrot-cdf.py
@@ -32,40 +32,57 @@ for application in APPLICATIONS:
                 )
 
 # plot CDF
-PLOT_CDF = False
+PLOT_CDF = True
 if PLOT_CDF:
     bins = np.arange(0, 90 + 1, 1)
 
     for application in APPLICATIONS:
-        plt.figure(figsize=(12, 6))
+        # plt.figure(figsize=(12, 6))
+
+        value_50 = {}
+        value_90 = {}
         for condition in CONDITIONS:
             x = np.sort(d[application][condition])
             hist, bin_edges = np.histogram(x, bins=bins, density=True)
 
             cdf = np.cumsum(hist * np.diff(bin_edges))
 
-            plt.plot(
-                bin_edges[:-1],
-                cdf * 100,
-                linestyle="-",
-                color=COLORS[condition][DARKEST],
-                label=condition,
-            )
+            value_50.setdefault(condition, np.interp(0.5, cdf, bin_edges[:-1]))
+            value_90.setdefault(condition, np.interp(0.9, cdf, bin_edges[:-1]))
 
-        plt.xlabel("Head Rotation Angle (°)")
-        plt.ylabel("Cumulative Proportion (%)")
-        plt.title(
-            f"Cumulative Distribution Function of Head Rotation Angles ({application})"
+            # plt.plot(
+            #     bin_edges[:-1],
+            #     cdf * 100,
+            #     linestyle="-",
+            #     color=COLORS[condition][DARKEST],
+            #     label=condition,
+            # )
+
+        # plt.xlabel("Head Rotation Angle (°)")
+        # plt.ylabel("Cumulative Proportion (%)")
+        # plt.title(
+        #     f"Cumulative Distribution Function of Head Rotation Angles ({application})"
+        # )
+        # plt.xticks(np.arange(0, 90 + 10, 10))
+        # plt.yticks(np.arange(0, 100 + 10, 10))
+        # plt.legend()
+
+        # calculate the gap between two CDFs at 50% and 90%
+
+        gap_50 = np.abs(value_50[CONDITIONS[0]] - value_50[CONDITIONS[1]])
+        gap_90 = np.abs(value_90[CONDITIONS[0]] - value_90[CONDITIONS[1]])
+
+        logger.PRINT_LOG(
+            "INFO",
+            bcolors.OKGREEN,
+            f"{application}: gap at 50% = {gap_50:.5f}, gap at 90% = {gap_90:.5f}",
         )
-        plt.xticks(np.arange(0, 90 + 10, 10))
-        plt.yticks(np.arange(0, 100 + 10, 10))
-        plt.legend()
 
-        output_file = f"{OUTPUT_DIR}/{application}_headrot_cdf_(anglex).png"
-        plt.savefig(output_file)
-        plt.close()
+        # output_file = f"{OUTPUT_DIR}/{application}_headrot_cdf_(anglex).png"
+        # plt.savefig(output_file)
+        # plt.close()
 
-        logger.PRINT_LOG("SAVE PLOT", bcolors.OKBLUE, f"saved {output_file}")
+        # logger.PRINT_LOG("SAVE PLOT", bcolors.OKBLUE, f"saved {output_file}")
 
 # plot PDF
 PLOT_PDF = False
@@ -102,17 +119,18 @@ if PLOT_PDF:
 
         logger.PRINT_LOG("SAVE PLOT", bcolors.OKBLUE, f"saved {output_file}")
 
-
 # ks test
-from scipy.stats import ks_2samp
+KS_TEST = False
+if KS_TEST:
+    from scipy.stats import ks_2samp
 
-for application in APPLICATIONS:
-    x1 = d[application][CONDITIONS[0]]
-    x2 = d[application][CONDITIONS[1]]
+    for application in APPLICATIONS:
+        x1 = d[application][CONDITIONS[0]]
+        x2 = d[application][CONDITIONS[1]]
 
-    ks_stat, ks_pval = ks_2samp(x1, x2)
-    logger.PRINT_LOG(
-        "KS TEST",
-        bcolors.OKCYAN,
-        f"{application}: ks_stat={ks_stat:.5f}, ks_pval={ks_pval:.5f}",
-    )
+        ks_stat, ks_pval = ks_2samp(x1, x2)
+        logger.PRINT_LOG(
+            "INFO",
+            bcolors.OKCYAN,
+            f"{application}: ks_stat={ks_stat:.5f}, ks_pval={ks_pval:.5f}",
+        )
